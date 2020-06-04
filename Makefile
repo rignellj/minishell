0427ecfc19df4b80ff8f55c66afe24f79980fefe
Jere Rignell <jrignell@student.hive.fi>
2020-06-04 19:28:22 +0300

    update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jrignell <jrignell@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/13 18:19:52 by jrignell          #+#    #+#              #
-#    Updated: 2020/05/07 16:22:29 by jrignell         ###   ########.fr        #
+#    Updated: 2020/05/27 18:00:54 by jrignell         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,21 +20,26 @@ OBJ_DIR := ./obj
 
 SRC :=	main.c \
 		sh_env.c \
+		sh_init.c \
 		sh_exit.c \
 		minishell.c \
 		sh_change_dir.c \
 		sh_del_struct.c \
 		sh_init_struct.c \
+		sh_del_command.c \
 		sh_form_struct.c \
 		sh_trim_command.c \
 		sh_builtin_exec.c \
 		sh_check_builtin.c \
 		sh_display_prompt.c \
-		sh_read_more_input.c
+		sh_read_more_input.c \
+		sh_init_signal_handlers.c
 		
 OBJ :=	$(addprefix $(OBJ_DIR)/,$(SRC:.c=.o))
 
 all: $(NAME)
+
+# .SILENT:
 
 $(OBJ_DIR):
 	@/bin/mkdir -p $(OBJ_DIR)
@@ -43,22 +48,30 @@ $(OBJ_DIR)/%.o:$(SRC_DIR)/%.c
 	@$(CC) $(CFLAGS) -I $(INC_DIR) -o $@ -c $<
 
 $(NAME): $(OBJ_DIR) $(OBJ)
+	@echo "\\033[1;33mBuilding libft..\\033[0;39m"
 	@make -C libft/printf_srcs/
+	@echo "\\033[1;33mBuilding shell..\\033[0;39m"
 	@$(CC) $(CFLAGS) -o $(NAME) $(OBJ) libft/printf_srcs/libftprintf.a -ltermcap
+	@echo "\\033[1;32mDONE\\033[0;39m"
+	@echo "\n\\033[1;37mLaunch ./minishell\\033[0;39m"
 
 clean:
+	@echo "\\033[1;31mCleaning shell object files..\\033[0;39m"
 	@/bin/rm -f $(OBJ)
+	@echo "\\033[1;31mCleaning ft_printf and libft object files..\\033[0;39m"
 	@make -C libft/printf_srcs/ clean
 	@make -C libft/ clean
 
 fclean: clean
+	@echo "\n\\033[1;37mCleaning executables..\\033[0;39m"
 	@/bin/rm -f $(NAME)
-	@/bin/rm -f $(OBJ)
 	@make -C libft/printf_srcs/ fclean
 	@make -C libft/ fclean
 	
 sh: $(OBJ_DIR) $(OBJ)
+	@echo "\\033[1;33mBuilding project..\\033[0;39m"
 	@$(CC) $(CFLAGS) -o $(NAME) $(OBJ) libft/printf_srcs/libftprintf.a -ltermcap
+	@echo "\\033[1;32mDONE\\033[0;39m\n"
 	@make clean
 
 re: fclean all
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,34 +6,48 @@
 /*   By: jrignell <jrignell@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/23 19:38:34 by jrignell          #+#    #+#             */
-/*   Updated: 2020/05/07 19:10:52 by jrignell         ###   ########.fr       */
+/*   Updated: 2020/05/26 14:39:01 by jrignell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../libft/printf_srcs/includes/ft_printf.h"
+# include <termios.h>
 
 /*
 ********************************************************************************
-**                                                                            **
-**                         DEV                                                **
-**                                                                            **
+**                         KEYS                                               **
 ********************************************************************************
 */
+
+# define LEFT 0x445B1B
+# define RIGHT 0x435B1B
+# define DOWN 0x425B1B
+# define UP 0x415B1B
+# define ENTER 0xA
+# define ESC 0x1B
+# define DEL 0x7E335B1B
+# define SPACE 0x20
+# define BSPACE 0x7F
+# define ASTERISK 0x2A
+# define MINUS 0x2D
+
 /*
 ********************************************************************************
-**                                                                            **
 **                         STRUCTS                                            **
-**                                                                            **
 ********************************************************************************
 */
+
+typedef struct termios	t_termios;
 
 typedef struct	s_shell
 {
 	int			ac;
-	int			fd;
+	int			term_fd;
+	int			file_fd;
 	int			i;
+	pid_t		child_status;
 	char		**av;
 	char		**env;
 	char		**commands; //doesn't need to be freed -> minishell function handles this
@@ -41,8 +55,17 @@ typedef struct	s_shell
 	char		**built_ins;
 	char		*exec_path;
 	char		*user_command;
+	t_termios	original_term_mode;
+	t_termios	current_term_mode;
 }				t_shell;
 
+/*
+********************************************************************************
+**                         GLOBALS                                            **
+********************************************************************************
+*/
+
+t_shell			*g_shell;
 
 /*
 ********************************************************************************
@@ -52,11 +75,13 @@ typedef struct	s_shell
 ********************************************************************************
 */
 
+void			sh_init(int ac, char *av[], char *env[], t_shell *d);
+void			sh_init_signal_handlers(void);
 void			minishell(t_shell *info);
 char			*sh_read_line(t_shell *d);
 char			*sh_read_more_input(t_shell *d);
 int				sh_error(char *error);
-int				sh_exit(t_shell *d);
+int				sh_exit(t_shell *d, char const *error);
 void			sh_form_struct(t_shell *d);
 int				sh_display_prompt(t_shell *data);
 int				sh_trim_command(t_shell *d);
@@ -65,6 +90,7 @@ void			sh_init_struct(t_shell *data);
 int				sh_check_builtin(t_shell *d, char *bic);
 int				sh_change_dir(t_shell *d);
 int				sh_env(t_shell *d, size_t len);
+void			sh_del_command(t_shell *d, char *line, char **c);
 void			sh_del_struct(t_shell *d);
 
 #endif
